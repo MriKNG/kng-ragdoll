5b8ff6b2c8079e7ce6495d491381c1ac2f300d09
KNG <108199777+MriKNG@users.noreply.github.com>
2023-03-03 21:29:35 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,6 @@ author "KNG"
 version "1.0"
 
 client_scripts {
-    "cl_keybind.lua", --TO USE KEYBIND
-    --"cl_command.lua" --UNCOMMENT TO USE COMMAND
+    "cl_keybind.lua", ---TO USE KEYBIND
+    --"cl_command.lua" ---UNCOMMENT TO USE COMMAND
 }
